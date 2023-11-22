9a632d4936268d4234169a6a23c541cab6da5424
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-11-22 22:39:30 +0530

    added new famous player page and made some changes in the code

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+
     <head>
-        <link rel="stylesheet" href="css/style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       
+        <link rel="stylesheet" href="css/style.css" >
         <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+        <title>Ballor Talk</title>
+        <link rel="icon" type="image/x-icon" href="img/4_teams.ico">
     </head>
     <body>
-        <header class="header" id="home">
+        <header class="header" id="home" style="background-image: url('img/stadium.jpeg'); background-repeat: no-repeat;">
             <div class="nav">
                 <div class="navigation container">
                     <div class="logo">
-                        <h1>BallorTalk</h1>
+                    <a href="index.php"><h1>BallorTalk</h1></a>
                     </div>
                     <div class="menu">
                         <div class="top-nav">
@@ -27,129 +35,161 @@
                                 <a href="#about" class="nav-link">About</a>
                             </li>
                             <li class="nav-item">
-                                <a href="#team" class="nav-link">Team</a>
+                                <a href="#teams" class="nav-link">Team</a>
                             </li>
                             <li class="nav-item">
+                                <a href="news.php" class="nav-link">News</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="players.php" class="nav-link">Famous_Players</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="tribute.php" class="nav-link">Tribute</a>
+                            </li>
+                            <li class="nav-item">
+
                                 <a href="#contact" class="nav-link">Contact</a>
                             </li>
+                           
                             <li class="nav-item">
                                 <a href="/Minorproject/login.php" class="nav-link">Log in</a>
                             </li>
                         </ul>
                     </div>
-                    <div class="hamburger">
-                        <i class="fa fa-bars"></i>
-                    </div>
+                   
                 </div>
             </div>
-            <img src="img/4.png" alt="">
+            <img src="img/1.png" alt="">
             <div class="hero">
                 <h3>A football statistics website</h3>
-                <h1>The world of Football at your fingertips</h1>
+                <h2>The world of Football at your fingertips</h2>
                 
-                <a href="#">Hire me</a>
+                
             </div>
            <div class="social-link">
-               <i class="fa fa-facebook"></i>
-               <i class="fa fa-instagram"></i>
-               <i class="fa fa-twitter"></i>
-               <i class="fa fa-linkedin"></i>
-               <i class="fa fa-telegram"></i>
+               <a href="https://www.instagram.com/anshsharma_msd/"><i class="fa fa-facebook"></i></a>
+               <a href="https://www.instagram.com/anshsharma_msd/"><i class="fa fa-instagram"></i></a>
+               <a href="https://www.instagram.com/anshsharma_msd/"><i class="fa fa-twitter"></i></a>
+               <a href="https://www.instagram.com/anshsharma_msd/"><i class="fa fa-linkedin"></i></a>
+               <a href="https://www.instagram.com/anshsharma_msd/"><i class="fa fa-telegram"></i></a>
            </div>
         </header>
         <main>
             <section class="section about" id="about">
                 <div class="title">
-                    <h1>About Me</h1>
+                    <h1>About Us</h1>
                 </div>
                 <div class="about-center container">
                     <div class="left">
                         <img src="img/6.png" alt="">
                     </div>
                     <div class="right">
-                        <h1>I'm Nemar Footballer</h1>
+                        <h1>Ballor Talk - We provide each and every information about football </h1>
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, voluptate! Mollitia, 
-                            perspiciatis praesentium. Consequatur necessitatibus iste mollitia, velit, 
-                            maiores vel odit harum, eaque dolore quo similique possimus impedit praesentium placeat!
+                            Ballor Talk is derived from Ballon d'Or which is an annual football award,
+                            given to the best player over the previous year. 
+                            The award was first presented in 1956 by the French magazine France Football. 
+                            The name translates to "Golden Ball" in English.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, voluptate! Mollitia, 
-                            perspiciatis praesentium. Consequatur necessitatibus iste mollitia, velit, 
-                            maiores vel odit harum, eaque dolore quo similique possimus impedit praesentium placeat!
+                            
                         </p>
-                        <a href="#" class="btn">Download CV</a>
+                        <a href="#" class="btn">Back to top</a>
                     </div>
                 </div>
             </section>
         </main>
         <section class="section them" id="service">
             <div class="title">
-                <h1>My Services</h1>
+                <h1>Our Services</h1>
             </div>
             <div class="service-center">
                 <div class="service">
-                    <span class="fa fa-user"></span>
-                    <h2>Training</h2>
+                    <span class="fa fa-futbol-o"></span>
+                    <h2>Live Scores</h2>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, 
-                        doloribus ducimus odit libero doloribus ducimus odit libero.
+                    Live football scores, serves fresh and real-time, 
+                    paint the vivid picture of the game's unfolding drama, 
+                    keeping fans connected to every thrilling moment on the field.
                     </p>
                 </div>
                 <div class="service">
-                    <span class="fa fa-users"></span>
-                    <h2>Playing</h2>
+                    <span class="fa fa-line-chart"></span>
+                    <h2>Analytics</h2>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, 
-                        doloribus ducimus odit libero doloribus ducimus odit libero.
+                    Live football analytics harnesses real-time data, providing insightful  patterns, 
+                    empowering teams and enthusiasts with strategic insights.
                     </p>
                 </div>
                 <div class="service">
                     <span class="fa fa-street-view"></span>
-                    <h2>Group Master</h2>
+                    <h2>Tournaments</h2>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, 
-                        doloribus ducimus odit libero doloribus ducimus odit libero.
+                    Live football tournaments are grand stages where passion meets skill, 
+                    uniting fans worldwide in a sense of anticipation and unforgettable moments,
+                    painting the field with the colors of dreams 
                     </p>
                 </div>
+                <div class="service">
+                    <span class="fa fa-user-o"></span>
+                    <h2>Famous Players</h2>
+                    <p>
+                    Famous football players inspire millions with their skill, dedication, and remarkable achievements, 
+                    shaping the game's history with their talent on and off the field. 
+                    
+                    </p>
+                </div>
+
+                <div class="service">
+                    <span class="fa fa-address-card-o"></span>
+                    <h2>Tribute to Legends</h2>
+                    <p>
+                    In homage to legends, their footprints echo on fields,
+                     immortalizing their grace, skill, and indelible impact on the beautiful game. 
+                     Tributes unveil a mosaic of brilliance, honoring the legacy of iconic football virtuosos.
+                    </p>
+                </div>
+
+                
             </div>
         </section>
         <section class="section skills" id="skills">
             <div class="title">
-                <h1>My Skills</h1>
+                <h1> Skills That are must in a football Player</h1>
+                <h1>will make changes in this section</h1>
             </div>
             <div class="skills-center container">
                 <div class="skill-left">
                     <div class="skill-box">
-                        <h4>Playing</h4>
+                        <h4>Still Working</h4>
                         <div class="skill-ilt">
                             <div class="skill-bar playing"></div>
-                            <span>95%</span>
+                            <span>60%</span>
                         </div>
                     </div>
                     <div class="skill-box">
-                        <h4>Shooting</h4>
+                        <h4>Still Workingng</h4>
                         <div class="skill-ilt">
                             <div class="skill-bar shooting"></div>
                             <span>90%</span>
                         </div>
                     </div>
                     <div class="skill-box">
-                        <h4>Passing</h4>
+                        <h4>Still Workingg</h4>
                         <div class="skill-ilt">
                             <div class="skill-bar passing"></div>
                             <span>80%</span>
                         </div>
                     </div>
                     <div class="skill-box">
-                        <h4>Drible</h4>
+                        <h4>Still Working</h4>
                         <div class="skill-ilt">
                             <div class="skill-bar drible"></div>
                             <span>95%</span>
                         </div>
                     </div>
                     <div class="skill-box">
-                        <h4>Goling</h4>
+                        <h4>Still Working</h4>
                         <div class="skill-ilt">
                             <div class="skill-bar golling"></div>
                             <span>70%</span>
@@ -158,29 +198,25 @@
                 </div>
                 <div class="skill-right">
                     <h3>My Ceartive Skills and Experience</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        A voluptates maxime quidem maiores? Laboriosam hic, repudiandae, 
-                        possimus suscipit aperiam consectetur veritatis officia placeat vel 
-                        voluptate eos nesciunt minima quis facere ipsum sed dicta excepturi 
-                        itaque. Voluptatibus recusandae amet eos accusantium, 
-                        perferendis autem aspernatur consequuntur incidunt quae, magni iste optio. Quos.
+                    <p>Currently work on it
                     </p>
-                    <a href="#" class="btn">Read More</a>
+                    <a href="#home" class="btn">Read More</a>
                 </div>
             </div>
         </section>
-        <section class="section them" class teams>
-            <div class="title">
-                <h1>My Teams</h1>
+        <section class="section them" class teams id="teams" style="background-image: url('img/4_teams.jpeg');">
+            <div class="title" >
+                <h1 style="color:yellow; font-weight:bold"><span style="baackground-color:black;">Teams at the Apex</span></h1>
             </div>
-            <div class="team-center container">
+            <div class="team-center container" >
                 <div class="team">
                     <div class="img-cover">
-                        <img src="img/3.png" alt="">
+                        <img src="img/fcb.jpeg" alt="">
                     </div>
-                    <h3>Player Name</h3>
+                    <h3>Football Club Barcelona</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, consectetur.
+                    Barcelona is a famous soccer team known for playing amazing football. 
+                     The team wears blue and red colors and is loved by people all around the world for their skillful and exciting style of playing.
                     </p>
                     <div class="stars">
                         <i class="fa fa-star"></i>
@@ -192,11 +228,13 @@
                 </div>
                 <div class="team">
                     <div class="img-cover">
-                        <img src="img/1.png" alt="">
+                        <img src="img/chelsea.png" alt="">
                     </div>
-                    <h3>Player Name</h3>
+                    <h3>Chelsea</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, consectetur.
+                    Chelsea Football Team plays soccer. They're really good and win lots of games. 
+                    Their players wear blue uniforms. 
+                    Chelsea's won big trophies and is known for playing exciting matches.
                     </p>
                     <div class="stars">
                         <i class="fa fa-star"></i>
@@ -208,11 +246,32 @@
                 </div>
                 <div class="team">
                     <div class="img-cover">
-                        <img src="img/5.png" alt="">
+                        <img src="img/mu.jpeg" alt="">
                     </div>
-                    <h3>Player Name</h3>
+                    <h3>Manchester United</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, consectetur.
+                    Manchester United is a famous football team from England.
+                     They've won many trophies and have lots of fans around the world. 
+                     Their red jerseys and Old Trafford stadium are well-known. 
+                     
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+                <div class="team">
+                    <div class="img-cover">
+                        <img src="img/rm.png" alt="">
+                    </div>
+                    <h3>Real Madrid</h3>
+                    <p>
+                    Real Madrid, a famous soccer team, shines bright with lots of wins and famous players.
+                    fans love their exciting games.
+                    With a rich history and a tradition of winning, they're a powerhouse in the world of football.
                     </p>
                     <div class="stars">
                         <i class="fa fa-star"></i>
@@ -225,23 +284,25 @@
             </div>
         </section>
         <section class="section contact" id="contact">
+        
             <div class="title">
-                <h1>Contact Me</h1>
+                <h1>Contact Us</h1>
             </div>
+            
             <div class="contact-center container">
                 <div class="left">
                     <h2>Get In Touch</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Sequi velit quisquam maxime minus minima optio architecto? 
-                        Illo rem ipsam possimus, omnis in, eligendi, 
-                        laboriosam odio id beatae corporis iusto praesentium!
+                        if have any doubts .<br> 
+                        or simply wants to get in touch ,<br>
+                        you may use the contact form !
+                        
                     </p>
                     <div>
                         <span class="icon"><i class="fa fa-user"></i></span>
                         <span class="content">
                             <h3>Name</h3>
-                            <span>Nemar</span>
+                            <span>bellortalk</span>
                         </span>
                     </div>
                     <div>
@@ -255,28 +316,28 @@
                         <span class="icon"><i class="fa fa-envelope"></i></span>
                         <span class="content">
                             <h3>Email</h3>
-                            <span>Nemay@gmail.com</span>
+                            <span>ballortalk@gmail.com</span>
                         </span>
                     </div>
                 </div>
                 <div class="right">
-                    <form action="" class="form">
-                        <h2>Message Me</h2>
+                    <form action="mailto:'ballortalk@gmail.com'" class="form">
+                        <h2>Contact us</h2>
                         <div>
                             <input type="text" placeholder="Name">
                             <input type="text" placeholder="Email">
                         </div>
                         <input type="text" placeholder="Subject">
                         <textarea name="" id="" cols="30" rows="10" placeholder="Describe Messeage">
+                            
 
                         </textarea>
+                        <button type="submit" class="btn btn-success">Submit</button>
                     </form>
                 </div>
             </div>
         </section>
-        <footer>
-            <p>Created By<span>codeSkill</span> | &copy; 2021 All Right Reserved</p>
-        </footer>
+       
     </body>
     <script src="js/script.js"></script>
     
---
+++ players.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       
+        <link rel="stylesheet" href="css/style.css" >
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+        <title>Famous Players</title>
+    </head>
+    <body>
+    <header class="header" id="home" style=" height: 100px; width: 100%;">
+            <div class="nav">
+                <div class="navigation container">
+                    <div class="logo">
+                     <a href="index.php"><h1>BallorTalk</h1></a>
+                    </div>
+                    <div class="menu">
+                        <div class="top-nav">
+                            <div class="logo">
+                                <h1>BallorTalk</h1>
+                            </div>
+                            <div class="close">
+                                <i class="fa fa-times"></i>
+                            </div>
+                        </div>
+                        <ul class="nav-list">
+                            <li class="nav-item">
+                                <a href="#home" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#about" class="nav-link">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#teams" class="nav-link">Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="news.php" class="nav-link">News</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="players.php" class="nav-link">Famous_Players</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="tribute.php" class="nav-link">Tribute</a>
+                            </li>
+                            <li class="nav-item">
+
+                                <a href="#contact" class="nav-link">Contact</a>
+                            </li>
+                           
+                            <li class="nav-item">
+                                <a href="/Minorproject/login.php" class="nav-link">Log in</a>
+                            </li>
+                        </ul>
+                    </div>
+                   
+                </div>
+            </div>
+            <!-- <img src="img/3.png" alt=""> -->
+            <!-- <div class="row">
+                <div class="col">
+                    <img src="img/1.png" alt="">
+                </div>
+                <div class="col">
+                    <img src="img/famous_players_quote.png" alt="">
+                </div>
+            </div> -->
+           
+
+            
+           
+        </header>
+
+        
+            
+
+        <section class="section them"  id="teams">
+            <div class="title">
+                <h1>Famous Players all over the world</h1>
+            </div>
+            <div class="team-center container">
+                <div class="team" style="background-image: url('img/fcb.jpeg');">
+                    <div class="img-cover">
+                        <img src="img/1.png" alt="">
+                    </div>
+                    <h3>Lionel Messi</h3>
+                    <p style="background-color:black;">
+                        Lionel Messi plays soccer amazingly well.
+                         He's really famous and scores lots of goals.
+                          People love watching him play because he's so good. 
+                          He's nice and works hard, making him a big star in soccer..
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        
+                    </div>
+                </div>
+                <div class="team" style="background-image: url('img/rm.png');">
+                    <div class="img-cover">
+                        <img src="img/3.png" alt="">
+                    </div>
+                    <h3>Cristiano Ronaldo</h3>
+                    <p style="background-color:black;">
+                    Cristiano Ronaldo, a football star ,He's famous worldwide for his skills and hard work, scoring lots of goals for his teams.
+                     Off the field, he's a role model, known for his charity work and dedication to the game, making him a hero to many fans.
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+                <div class="team" style="background-image: url('img/mu.jpeg');">
+                    <div class="img-cover">
+                        <img src="img/david.jpeg" alt="">
+                    </div>
+                    <h3>David Beckham</h3>
+                    <p style="background-color:black;">
+                    David Beckham, known for his style on and off the field, was a great soccer player with a powerful kick and precise passes .
+                     His charisma made him a global superstar, left an enduring impact on football's history.
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+                <div class="team" style="background-image: url('img/inida.png');">
+                    <div class="img-cover">
+                        <img src="img/sunil chettri.jpeg" alt="">
+                    </div>
+                    <h3>Sunil Chhetri</h3>
+                    <p style="background-color:black;">
+                    Sunil Chhetri,an Indian football star, shines brightly with his goals and leadership on the field.As India's all-time leading scorer, he's a symbol of inspiration and dedication to the sport. 
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+                <div class="team" style="background-image: url('img/4_teams.jpeg');">
+                    <div class="img-cover">
+                        <img src="img/3.png" alt="">
+                    </div>
+                    <h3>Player Name</h3>
+                    <p style="background-color:black;">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, consectetur.
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+                <div class="team" style="background-image: url('img/4_teams.jpeg');">
+                    <div class="img-cover">
+                        <img src="img/3.png" alt="">
+                    </div>
+                    <h3>Player Name</h3>
+                    <p style="background-color:black;">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, consectetur.
+                    </p>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+       
+    </body>
+    <script src="js/script.js"></script>
+    
+</html>
